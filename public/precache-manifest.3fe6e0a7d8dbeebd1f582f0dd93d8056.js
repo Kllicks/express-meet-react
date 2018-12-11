0011d89449fa0516930769169297db8d0835ab50
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-12-11 14:24:23 -0500

    express react app ready to deploy

---
+++ public/precache-manifest.3fe6e0a7d8dbeebd1f582f0dd93d8056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2bb5a1d3337e2033d1c9",
+    "url": "/static/js/main.2bb5a1d3.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "2bb5a1d3337e2033d1c9",
+    "url": "/static/css/main.80b42d46.chunk.css"
+  },
+  {
+    "revision": "23694fb402db9592817195a23ee37cfc",
+    "url": "/index.html"
+  }
+];
